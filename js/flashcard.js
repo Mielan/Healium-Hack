eaf8e628d0e5a3a8230bc02bb184c1fb8dad9cc8
Mielan <Mielan@users.noreply.github.com>
2019-08-18 14:08:18 -0700

    change

--- js/flashcard.js
+++ js/flashcard.js
@@ -1,20 +1,156 @@
 var flag = 1;
 
 var id = 0;
-var dataData = ["telencephalon, the most highly developed and anterior part of the forebrain; consisting chiefly of the cerebral hemispheres.",
-    "mesencephalon, The mesencephalon or midbrain is a part of the brain stem. It is associated with vision; hearing; motor control; sleep/wake; arousal (alertness); and temperature regulation.",
-    "midbrain, a small central part of the brainstem; developing from the middle of the primitive or embryonic brain.",
-    "prosencephalon, the anterior part of the brain, including the cerebral hemispheres; the thalamus, and the hypothalamus.", 
-    "rhombencephalon, The hindbrain or rhombencephalon is a developmental categorization of portions of the central nervous system in vertebrates.It includes the medulla, pons, and cerebellum.Together they support vital bodily processes.",
-    "neural, relating to a nerve or the nervous system",
-    "diencephalon, the caudal (posterior) part of the forebrain, containing the epithalamus, thalamus, hypothalamus, and ventral thalamus and the third ventricle.",
-    "ectoderm, the outermost layer of cells or tissue of an embryo in early development, or the parts derived from this, which include the epidermis, nerve tissue, and nephridia.",
-    "thalamus, either of two masses of grey matter lying between the cerebral hemispheres on either side of the third ventricle, relaying sensory information and acting as a centre for pain perception.",
-    "hypothalamus, a region of the forebrain below the thalamus which coordinates both the autonomic nervous system and the activity of the pituitary, controlling body temperature, thirst, hunger, and other homeostatic systems, and involved in sleep and emotional activity.",
-    "forebrain, the anterior part of the brain, including the cerebral hemispheres, the thalamus, and the hypothalamus.",
-    "vesicles, a small fluid-filled bladder, sac, cyst, or vacuole within the body.",
-    "ventricle, a hollow part or cavity in an organ.",
-    "embryonic, relating to an embryo"]
+var js=
+{
+    "transactionId": "d0b13e9b-2a5c-4128-ae37-ad09c86bdcaf",
+        "live": false,
+            "visitor": {
+        "metrics": {
+            "Weeks since first visit": 1.0,
+            "Total referred visits": 1.0,
+            "Total time spent on site in minutes": 0.3,
+            "Average visits per week": 1.0,
+            "PDP Pageviews - Visitor": 2.0,
+            "Total Time in Min / Weeks Since First Visit": 0.0,
+            "Orders - Last 30 Days": 0.0,
+            "Lifetime visit count": 1.0,
+            "Lifetime Sale Views": 1.0,
+            "Average visit duration in minutes": 0.3,
+             "Lifetime event count": 3.0,
+             "Orders - Last 60 Days": 0.0,
+             "Category Page Views - Visitor": 2.0
+        },
+        "dates": {
+            "Last visit": 1566087191000,
+                "last_visit_start_ts": 1566087173000,
+                    "audience_tealiumlabs-hackathon_sd-aug-2019-036_129_count_ts": 1566087191013,
+                        "audience_tealiumlabs-hackathon_sd-aug-2019-036_106_count_ts": 1566087173131,
+                            "audience_tealiumlabs-hackathon_sd-aug-2019-036_124_count_ts": 1566088994879,
+                                "audience_tealiumlabs-hackathon_sd-aug-2019-036_121_count_ts": 1566087173131,
+                                    "Last Visit Date": 1566088994879,
+                                        "First visit": 1566087173000,
+                                            "audience_tealiumlabs-hackathon_sd-aug-2019-036_113_count_ts": 1566087173131
+        },
+        "properties": {
+            "Count Product (URL) (favorite)": "http://www.tealiumecommerce.com/women/new-arrivals.html",
+                "Lifetime browser versions used (favorite)": "Chrome",
+                    "PDP Category Viewed - Most Recent": "Sale",
+                        "Lifetime browser types used (favorite)": "Chrome",
+                            "profile": "sd-aug-2019-036",
+                                "Lifetime devices used (favorite)": "Mac desktop",
+                                    "Category (favorite)": "Sale",
+                                        "Lifetime platforms used (favorite)": "browser",
+                                            "Adobe ID": "3BD4847EBD7FBC01-0517EF8537B7C396",
+                                                "Product URL": "http://www.tealiumecommerce.com/sale.html",
+                                                    "Active Device type (favorite)": "Mac desktop",
+                                                        "Last event URL": "http://www.tealiumecommerce.com/sale.html",
+                                                            "account": "tealiumlabs-hackathon",
+                                                                "Visitor utag_main_v_id": "016ca211d2830046422b627dd7980307800200700093c",
+                                                                    "Lifetime operating systems used (favorite)": "Mac OS X"
+        },
+        "flags": {
+            "Order Complete": false,
+                "Add to Cart": false,
+                    "Paid Search": false
+        },
+        "audiences": [
+            "Adobe Visitor Update",
+            "Abandoners - Browse",
+            "Every Event",
+            "All Users - For Splunk",
+            "Visitors - Unknown"
+        ],
+            "badges": [
+                "Sale Fan",
+                "Visitor - Unknown",
+                "Browse Abandoner"
+            ],
+                "property_sets": {
+            "Top 3 Product Categories": [
+                "Sale",
+                "New Arrivals"
+            ]
+        },
+        "metric_sets": {
+            "Lifetime operating systems used": {
+                "Mac OS X": 1.0
+            },
+            "Category": {
+                "Sale": 1.0,
+                    "New Arrivals": 1.0
+            },
+            "Active Device type": {
+                "Mac desktop": 3.0
+            },
+            "Lifetime devices used": {
+                "Mac desktop": 1.0
+            },
+            "Lifetime browser versions used": {
+                "Chrome": 1.0
+            },
+            "Count Product (URL)": {
+                "http://www.tealiumecommerce.com/women/new-arrivals.html": 1.0
+            },
+            "Lifetime browser types used": {
+                "Chrome": 1.0
+            },
+            "Lifetime platforms used": {
+                "browser": 1.0
+            }
+        },
+        "last_visit_id": "dc190425e4087a7af77c8b4a7e70777625207669a0de950e61dd93f6939b0d30"
+    }
+}
+
+var charValue1;
+charValue1= js.metrics["Weeks since first visit"];
+
+var chartName = [
+    "Women Views",
+    "Men Views",
+    "Accessories Views",
+    "Home & Decor Views",
+    "Sale Views",
+    "Weeks between Views",
+    "Purchase Views",
+];
+var chartValue = [
+    13,
+    34,
+    35,
+    67,
+    23,
+    45,
+    56,
+    67
+];
+
+var myMap = new Map();
+myMap.set(34, "Women Views");
+myMap.set(13, "Men Views");
+myMap.set(13, "Accessories Views");
+myMap.set(40, "Home & Decor Views");
+myMap.set(37, "Sale Views");
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+function updateScale() {
+    
+    //chart.options.data[0].dataPoints.push({ y: 25 - Math.random() * 10, name: "A" });
+    var i = Math.floor(Math.abs(Math.random() * 6));
+    var nameS = chartName[i];
+    var valueC = chartValue[i];
+    chart.options.data[0].dataPoints[i] = { y: valueC, name: nameS };
+    alert(charValue1);
+    chart.render();
+}
 
 function randomId() {
     var length = dataData.length;
